4feb28ccf4e49fa52c9cf5584c40eb6f79b2b29b
Sam <yedigaryansem@gmail.com>
2022-09-08 18:57:49 +0400

    Refactoring partial update classes to eliminate code duplication

--- gorest-test-framework-core/src/main/java/gorest/test/core/model/CommentPartialUpdate.java
+++ gorest-test-framework-core/src/main/java/gorest/test/core/model/CommentPartialUpdate.java
@@ -2,12 +2,14 @@ package gorest.test.core.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @SuperBuilder
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommentPartialUpdate extends CommentResource {
     public abstract static class CommentPartialUpdateBuilder<C extends CommentPartialUpdate, B extends CommentPartialUpdateBuilder<C, B>>
--- gorest-test-framework-core/src/main/java/gorest/test/core/model/UserPartialUpdate.java
+++ gorest-test-framework-core/src/main/java/gorest/test/core/model/UserPartialUpdate.java
@@ -2,12 +2,14 @@ package gorest.test.core.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @SuperBuilder
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserPartialUpdate extends UserResource {
     public abstract static class UserPartialUpdateBuilder<C extends UserPartialUpdate, B extends UserPartialUpdateBuilder<C, B>>
